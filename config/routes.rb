4926f507275697719bbbbd47d9eed3f980ea3438
Rohan Mendiratta <97857962+ROMENDI@users.noreply.github.com>
2024-02-26 22:28:34 +0000

    completed project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
+  resources :books
+  # Routes for the Movie resource:
+  root "movies#index"
+  # CREATE
+  post("/movies", { :controller => "movies", :action => "create" })
+          
+  # READ
+  get("/movies", { :controller => "movies", :action => "index" })
+  get("/movies/:id", { :controller => "movies", :action => "show" })
+  get "/movies/:id/edit", controller: "movies", action: "edit"
+  get("/movies/new", { :controller => "movies", :action => "new" })
+  
+  # UPDATE
+  patch("/movies/:id", { :controller => "movies", :action => "update" })
+  
+  # DELETE
+  delete("/movies/:id", { :controller => "movies", :action => "destroy" })
+
+  #------------------------------
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
